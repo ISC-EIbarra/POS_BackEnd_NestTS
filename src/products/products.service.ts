9b72e4a39a365ec33308f69625b20125f5d4a672
Edson <EdsonP4100@gmail.com>
2026-05-05 19:21:41 -0700

    seeder

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,7 +4,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { Category } from 'src/categories/entities/category.entity';
+import { Category } from '../categories/entities/category.entity';
 
 @Injectable()
 export class ProductsService {
