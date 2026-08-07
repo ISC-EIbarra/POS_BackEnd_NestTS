ee45deed5fb18bb978151c26207bd39652334e61
Edson <EdsonP4100@gmail.com>
2026-08-06 18:30:10 -0700

    Fix discount default value

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -18,7 +18,7 @@ export class Transaction {
   @Column({ type: 'varchar', length: 30, nullable: true })
   coupon: string;
 
-  @Column({ type: 'decimal', nullable: true })
+  @Column({ type: 'decimal', default: '0' })
   discount: number;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)' })
