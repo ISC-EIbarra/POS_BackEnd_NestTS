2a643b66ba6f0b40968df00812953329ed49f841
Edson <EdsonP4100@gmail.com>
2026-09-15 14:48:12 -0700

    Added upload-image service

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import 'multer';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
